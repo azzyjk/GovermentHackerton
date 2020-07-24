0a37ac88def1220a2c77749d95efcebc4177f6a7
azzyjk <56161518+azzyjk@users.noreply.github.com>
2020-07-24 16:37:11 +0900

    RN_2 : 알람이 울렸을 때 어플화면을 보고 있으면 alert로 내용 보여주는 기능

--- reactNative/App.js
+++ reactNative/App.js
@@ -5,7 +5,7 @@ import { Notifications } from "expo";
 import Constants from "expo-constants";
 import * as Permissions from "expo-permissions";
 
-async function getiOSNotificationPermission() {
+async function _getiOSNotificationPermission() {
   const { status } = await Permissions.getAsync(Permissions.NOTIFICATIONS);
   if (status !== "granted") {
     await Permissions.askAsync(Permissions.NOTIFICATIONS);
@@ -15,24 +15,26 @@ async function getiOSNotificationPermission() {
 export default class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      id: null,
-    };
+    this.state = {};
   }
 
   _handleButtonPress = () => {
+    const titles = ["오늘은 미세먼지가 나쁜 날이에요!"];
     const messages = [
       "test111111111",
       "test2222222222",
       "test3333333333",
       "test4444444444",
     ];
+
     const message = messages[Math.floor(Math.random() * messages.length)];
+    const title = titles[Math.floor(Math.random() * titles.length)];
 
     // push notification의 설정 및 정보
     const localnotification = {
-      title: "오늘은 미세먼지가 나쁜 날이에요!",
+      title: title,
       body: message,
+      data: { title: title, message: message },
       android: {
         sound: true,
       },
@@ -56,20 +58,17 @@ export default class App extends Component {
 
   _listenForNotifications = () => {
     Notifications.addListener((notification) => {
-      // console.log("notification title");
-      // console.log(notification.title);
-      // console.log("notification body");
-      // console.log(notification.body);
       if (notification.origin === "received" && Platform.OS === "ios") {
-        // Alert.alert(notification.title, notification.body);
-        Alert.alert("Test", "Testing");
+        Alert.alert(notification.data.title, notification.data.message);
       }
     });
   };
+
   componentDidMount() {
-    getiOSNotificationPermission();
+    _getiOSNotificationPermission();
     this._listenForNotifications();
   }
+
   render() {
     return (
       <View style={styles.container}>
