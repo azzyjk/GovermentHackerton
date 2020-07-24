d7756fc00d7edf67b52c8825904ed79df9e81f1c
junsik1998 <junsik1998@naver.com>
2020-07-24 22:33:18 +0900

    add buzzer

--- arduino/toilet/toilet.ino
+++ arduino/toilet/toilet.ino
@@ -3,6 +3,7 @@
 SoftwareSerial mySerial(2, 3);
 
 int motion_pin = 8;
+int buzzer_pin = 9;
 
 void setup() {
   Serial.begin(9600);
@@ -11,11 +12,21 @@ void setup() {
   mySerial.begin(9600);
 
   pinMode(motion_pin, INPUT);
+  pinMode(buzzer_pin, OUTPUT);
 }
 
 void loop() {
   if (mySerial.available()) { //블루투스에서 넘어온 데이터가 있다면
-    Serial.write(mySerial.read()); //시리얼모니터에 데이터를 출력
+    //Serial.write(mySerial.read()); //시리얼모니터에 데이터를 출력
+    char myChar = (char)mySerial.read();
+    Serial.write(myChar);
+    if(myChar == 'X'){
+      Serial.write("buzzer");
+      for(int freq = 150; freq <=1800; freq = freq + 2) {
+        tone(buzzer_pin, freq, 10);
+      }
+      myChar = ' ';
+    }
   }
   if (Serial.available()) {    //시리얼모니터에 입력된 데이터가 있다면
     mySerial.write(Serial.read());  //블루투스를 통해 입력된 데이터 전달
